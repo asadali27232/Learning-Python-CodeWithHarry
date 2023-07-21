44912eb4b483380c027204de9f82da13c551adc6
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-07-21 16:58:21 +0500

    Updated

--- Day 43 - Virtual Environment/main.py
+++ Day 43 - Virtual Environment/main.py
@@ -1,9 +1,20 @@
 # Create a virtual environment
-# python -m venv myenv
+# python -m venv my_venv
 
-# Activate the virtual environment (Windows)
+# Activate the virtual environment
 # myenv\Scripts\activate.bat
 
 import pandas
 
 print(pandas.__version__)
+
+# Deactivate the virtual environment
+# deactivate
+
+# Delete the virtual environment
+# rmdir my_venv /s
+
+# requirements.txt
+# pip freeze > requirements.txt
+
+# pip install -r requirements.txt
