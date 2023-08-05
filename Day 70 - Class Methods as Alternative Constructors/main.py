6652ebb267ece7ec81b88e97ae6d441534a20a77
Asad Ali <77291718+asadali27232@users.noreply.github.com>
2023-08-05 15:01:51 +0500

    Updated

---
+++ Day 70 - Class Methods as Alternative Constructors/main.py
@@ -0,0 +1,25 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    @classmethod
+    def from_string(cls, string):
+        name, age = string.split(",")
+        return cls(name, int(age))
+
+
+person = Person.from_string("John Doe, 30")
+
+
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    @classmethod
+    def square(cls, size):
+        return cls(size, size)
+
+
+rectangle = Rectangle.square(10)
